b700bcf3285cc24948e32372536f10836e9d28ec
Samuel Ramos <samuelsantos7@yahoo.com.br>
2021-05-24 16:13:57 -0300

    Configurações Babel/Webpack

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './global.css'
+import React from 'react'
+
+import backgroundImage from './assets/background.jpg'
+
+function App() {
+  return (
+    <div>
+      <img width={400} src={backgroundImage} alt="Background" />
+    </div>
+  )
+}
+
+export default App
